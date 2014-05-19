ebb1e77eae98a9c4c78755594bea30b782aa91ff
Chungsub Kim <chungsub.kim@purpleworks.co.kr>
2014-05-19 23:08:07 +0900

    init project

---
+++ README.md
@@ -0,0 +1,19 @@
+# angular-form-plus
+
+AngularJS directive for extending ng-form. 
+
+## Getting started
+
+...
+
+## Quick example
+
+...
+
+## Contributing
+
+1. Fork it ( https://github.com/remotty/angular-form-plus/fork )
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
---
+++ src/angular-form-plus.js
@@ -0,0 +1,56 @@
+/**!
+ * AngularJS Plupload directive
+ * @author Chungsub Kim <subicura@subicura.com>
+ */
+
+(function () {
+  'use strict';
+
+  angular.module('angular-form-plus', [])
+    .directive('formPlus', [
+      function () {
+        return {
+          restrict: 'A',
+          require: 'form',
+          /* jshint unused: false */
+          link: function postLink(scope, element, attrs) {
+            var form = element.controller('form');
+            var submit = attrs.ngSubmit;
+            element.unbind('submit');
+            element.bind('submit', function (event) {
+              form.submitted = true;
+              scope.$apply(submit);
+              if (!scope.$$phase) {
+                scope.$apply();
+              }
+            });
+
+            /**
+             * public methods
+             */
+
+            /**
+             * form initialize
+             */
+            form.init = function() {
+              form.$setPristine();
+              form.submitted = false;
+              if (!scope.$$phase) {
+                scope.$apply();
+              }
+            };
+
+            /**
+             * check invalid helper
+             * input창을 입력하지 않고 form을 서브밋 하거나 input창에 값을 입력했을 경우에만 체크한다.
+             * ex) form.isInvalid(title)
+             */
+            form.isInvalid = function(model) {
+              return (form.submitted || model.$dirty) && model.$invalid;
+            };
+          }
+        };
+      }
+    ]
+  );
+})();
